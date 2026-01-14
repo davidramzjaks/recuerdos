86a0e880c6472d188908638feda07236a717f8bc
davidramzjaks <deycrac@gmail.com>
2026-01-14 12:20:25 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const params = new URLSearchParams(window.location.search);
+const foto = params.get("img");
+
+if (foto) {
+  document.getElementById("foto").src = "fotos/" + foto;
+} else {
+  document.body.innerHTML = "<h2>No hay recuerdo para mostrar 😞</h2>";
+}
+
+
+const frases = [
+  "Lo bonito se queda aunque duela poquito 💛",
+  "Todo pasa por algo y tú también pasaste por algo 💫",
+  "Lo que un día te dolió hoy te hace más fuerte 🤍",
+  "Hay recuerdos que abrazan, otros que enseñan 🌿",
+  "Nada que valga la pena es fácil 💗",
+  "Si estás aquí, ya vas ganando 🕊️"
+];
+
+const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
+document.getElementById("texto-frase").textContent = fraseAleatoria;
